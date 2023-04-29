4f6150ae5574b06afd03c299c76cd45dbf0704cf
mister-kev <kf.me@icloud.com>
2023-04-29 12:49:19 -0400

    mobile optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,8 @@ const Home: NextPage = () => {
       <Head>
         <title>Round Table Ent. </title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-     
       <Hero />
 
       {/* <ImageLinks /> */}
