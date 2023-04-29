40b5715273ba3f33578fcc80474196ab30337604
mister-kev <kf.me@icloud.com>
2023-04-28 22:06:52 -0400

    Sidebar Animation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,3 @@
-
-//
 import React, { useState } from 'react';
 import RocketLaunchOutlinedIcon from '@mui/icons-material/RocketLaunchOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
@@ -18,35 +16,35 @@ const FloatingIcons: React.FC = () => {
   };
 
   return (
-    <div className={`fixed bottom-4 right-4 p-4  ${isCollapsed ? 'w-16' : 'w-20'}`}>
+    <div className={`fixed bottom-4 right-4 p-4 ${isCollapsed ? 'w-16' : 'w-20'}`}>
       <div className={`flex flex-col items-center justify-center space-y-4`}>
         {isCollapsed ? (
-          <button className="rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none" onClick={toggleCollapse}>
+          <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none" onClick={toggleCollapse}>
             <MenuIcon fontSize="medium" />
           </button>
         ) : (
-          <button className="rounded-full px-2 py-2  text-custom bg-white mb-2 focus:outline-none" onClick={toggleCollapse}>
-            <CloseIcon fontSize="large"/>
+          <button className="floating-animation rounded-full px-2 py-2 text-custom bg-white mb-2 focus:outline-none" onClick={toggleCollapse}>
+            <CloseIcon fontSize="large" />
           </button>
         )}
         {!isCollapsed && (
           <>
-            <button className="rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none">
               <InfoOutlinedIcon fontSize="medium" />
             </button>
-            <button className="rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mb-2 focus:outline-none">
               <EventNoteIcon fontSize="medium" />
             </button>
-            <button className="rounded-full px-3 py-3 text-black bg-white mb-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-3 py-3 text-black bg-white mb-2 focus:outline-none">
               <RocketLaunchOutlinedIcon fontSize="medium" />
             </button>
-            <button className="rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
               <AnnouncementOutlinedIcon fontSize="medium" />
             </button>
-            <button className="rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
               <GroupsOutlinedIcon fontSize="medium" />
             </button>
-            <button className="rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
+            <button className="floating-animation rounded-full px-2 py-2 text-black bg-white mt-2 focus:outline-none">
               <ShareOutlinedIcon fontSize="medium" />
             </button>
           </>
